6de10e16d50a95adf4407b6eee8e82a74cd18d0d
Vaibhav Sharma <vs8595257872@gmail.com>
2025-04-04 23:13:45 +0530

    New

---
+++ method.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class method {
+    public static void main(String[] args) {
+        String user = "vaibhav";
+        hello(user);
+    }
+    static void hello( String name){ 
+        System.out.println("hello "+ name );
+    }
+}
